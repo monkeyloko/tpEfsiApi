782c641728fb050e7eec400c67c506556fde3b20
JOAQUIN MACEIRA <47129158@ort>
2023-05-08 15:58:20 -0300

    agregar detalle

--- script.js
+++ script.js
@@ -7,72 +7,141 @@ function enviarForm(evento) {
     let titulo = document.getElementById("inputTitulo").value;
     let year = document.getElementById("inputAnio").value;
     let tipo = document.getElementById("selectTipo").value;
-    getMovie(titulo, year, tipo);
+    let season = document.getElementById("season").value;
+    let episode = document.getElementById("episode").value;
+
+
+    getMovie(titulo, year, tipo, season, episode);
 }
 
-function getMovie(titulo, year, tipo) {
-    console.log(year);
+function getMovie(titulo, year, tipo, season, episode) {
     tablaPelis.innerHTML="";
+    let isEpisode = (tipo == "episode")
     titulo = titulo.replace(/\s+/g, '_');
-
-    console.log(`https://www.omdbapi.com/?apikey=a004e2e7&s=${titulo}&y=${year}&type=${tipo}`)
-
-    const movieData = fetch(`https://www.omdbapi.com/?apikey=a004e2e7&s=${titulo}&y=${year}&type=${tipo}`)
+    console.log(`https://www.omdbapi.com/?apikey=a004e2e7&t=${titulo}&y=${year}&type=${tipo}`)
+    let appendix = "";
+    let ToS = "s";
+    if(isEpisode){
+        console.log("if")
+        appendix = `&season=${season}&episode=${episode}`
+        tipo = "series";
+        ToS = "t";
+    }
+    console.log(appendix)
+    let url = `https://www.omdbapi.com/?apikey=a004e2e7&${ToS}=${titulo}&y=${year}&type=${tipo}`
+    url += appendix;
+    console.log(url);
+    const movieData = fetch(url)
         .then(res => res.json())
         .then(res => {
             arr = res.Search;
-            console.log(arr);
-            if (arr != null) {
+            console.log(res.Response);   
+            if (res.Response) {
+                console.log("apex legends")
+                if(isEpisode == false){
+                    const tableHead = document.createElement("thead");
+                    const tableRow = document.createElement("tr");
+    
+                    const tableH = document.createElement("th");
+                    tableH.innerHTML = "Titulo";
+                    const tableH2 = document.createElement("th");
+                    tableH2.innerHTML = "Año";
+                    const tableH3 = document.createElement("th");
+                    tableH3.innerHTML = "Tipo";
+                    const tableH4 = document.createElement("th");
+                    tableH4.innerHTML = "ver";
+    
+                    tableRow.appendChild(tableH);
+                    tableRow.appendChild(tableH2);
+                    tableRow.appendChild(tableH3);
+                    tableRow.appendChild(tableH4);
+                    tableHead.appendChild(tableRow);
+    
+                    tablaPelis.appendChild(tableHead);
+    
+                    const tableBody = document.createElement("tbody");
+    
+                    arr.forEach(pelicula => {
+                        let peliculaId = pelicula.imdbID
+                        const peliRow = document.createElement("tr");
+                        const titulo = document.createElement("td");
+                        titulo.innerHTML = pelicula.Title;
+                        const year = document.createElement("td");
+                        year.innerHTML = pelicula.Year;
+                        const tipo = document.createElement("td");
+                        tipo.innerHTML = pelicula.Type;
+                        const detalleRow = document.createElement("td");
+                        const detalleBoton = document.createElement("a");
+                        detalleBoton
+    
+                        detalleBoton.classList.add("btn")
+                        detalleBoton.classList.add("btn-primary")
+                        detalleBoton.setAttribute("href", "detallePeli.html")
+                        
+                        peliRow.appendChild(titulo);
+                        peliRow.appendChild(year);
+                        peliRow.appendChild(tipo);
+                        peliRow.appendChild(detalleBoton)
+                        
+                        tableBody.appendChild(peliRow);
+                        
+    
+                    });
+                    tablaPelis.appendChild(tableBody);
+                }
+                else{
+                    const tableHead = document.createElement("thead");
+                    const tableRow = document.createElement("tr");
+                    
+    
+                    const tableH = document.createElement("th");
+                    tableH.innerHTML = "Titulo";
+                 
+                    const tableH2 = document.createElement("th");
+                    tableH2.innerHTML = "Año";
+                    const tableH3 = document.createElement("th");
+                    tableH3.innerHTML = "Tipo";
+                
+                    const tableH4 = document.createElement("th");
+                    tableH4.innerHTML = "Temporada";
 
-                const tableHead = document.createElement("thead");
-                const tableRow = document.createElement("tr");
+                    const tableH5 = document.createElement("th");
+                    tableH5.innerHTML = "Episodio";
 
-                const tableH = document.createElement("th");
-                tableH.innerHTML = "Titulo";
-                const tableH2 = document.createElement("th");
-                tableH2.innerHTML = "Año";
-                const tableH3 = document.createElement("th");
-                tableH3.innerHTML = "Tipo";
-               // const tableH4 = document.createElement("th");
-                //tableH3.innerHTML = "ver";
-
-                tableRow.appendChild(tableH);
-                tableRow.appendChild(tableH2);
-                tableRow.appendChild(tableH3);
-                //tableRow.appendChild(tableH4);
-                tableHead.appendChild(tableRow);
-
-                tablaPelis.appendChild(tableHead);
-
-                const tableBody = document.createElement("tbody");
-
-                arr.forEach(pelicula => {
-                    let peliculaId = pelicula.imdbID
+    
+                    tableRow.appendChild(tableH);
+                 
+                    tableRow.appendChild(tableH2);
+                    tableRow.appendChild(tableH3);
+                    tableRow.appendChild(tableH4);
+                    tableRow.appendChild(tableH5);
+                    tableHead.appendChild(tableRow);
+    
+                    tablaPelis.appendChild(tableHead);
+                    const tableBody = document.createElement("tbody");
                     const peliRow = document.createElement("tr");
-                    const titulo = document.createElement("td");
-                    titulo.innerHTML = pelicula.Title;
-                    const year = document.createElement("td");
-                    year.innerHTML = pelicula.Year;
-                    const tipo = document.createElement("td");
-                    tipo.innerHTML = pelicula.Type;
-                    /*const detalleRow = document.createElement("td");
-                    const detalleBoton = document.createElement("a");
-                    detalleBoton
+                        const titulo = document.createElement("td");
+                        titulo.innerHTML = res.Title;
+                        const year = document.createElement("td");
+                        year.innerHTML = res.Year;
+                        const tipo = document.createElement("td");
+                        tipo.innerHTML = res.Type;
+                        const temporada = document.createElement("td");
+                        temporada.innerHTML = res.Season;
+                        const Episodio = document.createElement("td");
+                        Episodio.innerHTML = res.Episode;
+                        console.log(titulo)
 
-                    detalleBoton.classList.add("btn")
-                    detalleBoton.classList.add("btn-primary")
-                    detalleBoton.setAttribute("href", "detallePeli.html")
-                    */
-                    peliRow.appendChild(titulo);
-                    peliRow.appendChild(year);
-                    peliRow.appendChild(tipo);
-                    //peliRow.appendChild(detalleRow)
-                    
-                    tableBody.appendChild(peliRow);
-                    
-
-                });
-                tablaPelis.appendChild(tableBody);
+                        peliRow.appendChild(titulo);
+                   
+                        peliRow.appendChild(year);
+                        peliRow.appendChild(tipo);
+                        peliRow.appendChild(temporada)
+                        peliRow.appendChild(Episodio)
+                        tableBody.appendChild(peliRow);
+                        tablaPelis.appendChild(tableBody);
+                }
+                
             }
             else {
                 console.log("fortnite");
