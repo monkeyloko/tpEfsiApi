7cb61123141e7e6255a0b14da16bb024eea92b1c
monkeyloko <defesanti@gmail.com>
2023-05-07 22:50:30 -0300

    working on script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-
-let divPelis = document.getElementById("divPelis");
+divPelis = document.getElementById("divPelis");
+let tablaPelis = document.getElementById("tablaPelis");
 document.getElementById("formPeliculas").addEventListener("submit", enviarForm);
 function enviarForm(evento) {
     evento.preventDefault()
@@ -18,7 +18,6 @@ function getMovie(titulo, year, tipo) {
 
     titulo = titulo.replace(/\s+/g, '_');
 
-
     console.log(`https://www.omdbapi.com/?apikey=a004e2e7&s=${titulo}&y=${year}&type=${tipo}`)
 
     const movieData = fetch(`https://www.omdbapi.com/?apikey=a004e2e7&s=${titulo}&y=${year}&type=${tipo}`)
@@ -32,28 +31,43 @@ function getMovie(titulo, year, tipo) {
                 const tableRow = document.createElement("tr");
 
                 const tableH = document.createElement("th");
-                tableH.innerHTML = "Titulo"
+                tableH.innerHTML = "Titulo";
                 const tableH2 = document.createElement("th");
-                tableH.innerHTML = "Año"
+                tableH2.innerHTML = "Año";
                 const tableH3 = document.createElement("th");
-                tableH.innerHTML = "Tipo"
+                tableH3.innerHTML = "Tipo";
 
-                tableHead.appendChild("tablaPelis")
-                tableRow.appendChild("thead")
+                tableRow.appendChild(tableH);
+                tableRow.appendChild(tableH2);
+                tableRow.appendChild(tableH3);
+                tableHead.appendChild(tableRow);
 
+                tablaPelis.appendChild(tableHead);
+
+                const tableBody = document.createElement("tbody");
 
                 arr.forEach(pelicula => {
-
-
-
-
+                    let peliculaId = pelicula.imdbID
+                    const peliRow = document.createElement("tr");
+                    const titulo = document.createElement("td");
+                    titulo.innerHTML = pelicula.Title;
+                    const year = document.createElement("td");
+                    year.innerHTML = pelicula.Year;
+                    const tipo = document.createElement("td");
+                    tipo.innerHTML = pelicula.Type;
+                    peliRow.appendChild(titulo);
+                    peliRow.appendChild(year);
+                    peliRow.appendChild(tipo);
+                    tableBody.appendChild(peliRow);
                 });
+                tablaPelis.appendChild(tableBody);
             }
             else {
-                console.log("fortnite")
+                console.log("fortnite");
             }
         })
         .catch(error => console.log(error))
-        .finally(() => console.log("terminé con el fetch"))
+        .finally(() => console.log("terminé con el fetch"));
 }
+
 
